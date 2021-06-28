f71405a4f332f4c675e5923f0991604c6598cd91
TarouTanakaYokohama <57135586+TarouTanakaYokohama@users.noreply.github.com>
2021-06-28 22:18:24 +0900

    qiita記事の取得完成

---
+++ src/lib/data/api/qiita_api_client.dart
@@ -0,0 +1,21 @@
+import 'dart:developer';
+import 'dart:math';
+
+import 'package:qiita_sample/data/entities/qiita_info.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:dio/dio.dart';
+
+part 'qiita_api_client.g.dart';
+
+@RestApi(baseUrl: "https://qiita.com/api/v2")
+abstract class QiitaApiClient {
+  factory QiitaApiClient(Dio dio, {String baseUrl}) = _QiitaApiClient;
+
+  static QiitaApiClient create() {
+    final dio = Dio();
+    return QiitaApiClient(dio);
+  }
+
+  @GET("/tags/flutter/items?per_page=50")
+  Future<List<QiitaInfo>> getFlutterArticles();
+}
