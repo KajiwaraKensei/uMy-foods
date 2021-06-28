1842ab4c8c8d34a1bd001469781439a4ed17f861
Rick2480 <kd1286212@st.kobedenshi.ac.jp>
2021-06-28 17:03:46 +0900

    Co-authored-by: TarouTanakaYokohama <TarouTanakaYokohama@users.noreply.github.com> Co-authored-by: Shulk171 <Shulk171@users.noreply.github.com>

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +23,45 @@ const MaterialColor customSwatch = const MaterialColor(
     900: const Color(0xFFEEEAE4),
   },
 );
+final List<Map<String, dynamic>> listItems = [
+  {
+    'Text': 'カントリーマアム1',
+    'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
+  },
+  {
+    'Text': 'カントリーマアム2',
+    'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
+  },
+  {
+    'Text': 'カントリーマアム3',
+    'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
+  },
+  {
+    'Text': 'パイの実',
+    'image':
+        'https://www.tanomail.com/imgcv/product/26/2621344_01.jpg?sr.dw=230&sr.dh=230',
+  },
+  {
+    'Text': 'カントリーマアム4',
+    'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
+  },
+  {
+    'Text': 'カントリーマアム5',
+    'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
+  },
+  {
+    'Text': 'カントリーマアム6',
+    'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
+  },
+  {
+    'Text': 'カントリーマアム7',
+    'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
+  },
+  {
+    'Text': 'カントリーマアム8',
+    'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
+  },
+];
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -82,39 +123,69 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  final List<Map<String, dynamic>> _listItems = [
-    {
-      'Text': 'カントリーマアム1',
-      'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
-    },
-    {
-      'Text': 'カントリーマアム2',
-      'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
-    },
-    {
-      'Text': 'カントリーマアム3',
-      'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
-    },
-    {
-      'Text': 'パイの実',
-      'image':
-          'https://www.tanomail.com/imgcv/product/26/2621344_01.jpg?sr.dw=230&sr.dh=230',
-    },
-    {
-      'Text': 'カントリーマアム4',
-      'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
-    },
-    {
-      'Text': 'カントリーマアム5',
-      'image': 'https://www.toysrus.co.jp/i/5/0/8/508961100AML.jpg',
-    },
-  ];
-
+  Widget ListItemWidget = Text(""); //表示用ウィジェット
   String _textex = "";
 
-  void _deleteItem(i) async {
+  //比較リストを返す
+  SingleChildScrollView showListItems() {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          // 3行
+          for (int x = 0; x < 3; x++)
+            Row(
+              children: [
+                // 横に4個並べる  4を変えれば変わる
+                // i = 0 +(x * 4) ・・・2列目なら4から、3列目なら8から
+                // i < 4 * (x + 1) ・・・2列目なら8まで、3列目なら12まで
+                for (int i = 0 + (x * 4); i < 4 * (x + 1); i++)
+                  if (listItems.length > i)
+                    Card(
+                      //margin: const EdgeInsets.all(10.0),
+                      child: Container(
+                        width: 180.0,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            ButtonTheme(
+                              child: ButtonBar(
+                                children: [
+                                  Tooltip(
+                                    message: 'クリップボードから削除します。',
+                                    child: IconButton(
+                                      icon: const Icon(Icons.close,
+                                          color: Colors.black),
+                                      onPressed: () {
+                                        _deleteItem(i);
+                                      },
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Image.network(
+                              listItems[i]['image'],
+                              height: 120.0,
+                              width: 120.0,
+                            ),
+                            Text(
+                              listItems[i]['Text'],
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+
+  void _deleteItem(i) {
     setState(() {
-      _listItems.removeAt(i);
+      listItems.removeAt(i); // 削除
     });
   }
 
@@ -130,154 +201,255 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ])),
       drawer: Drawer(child: Center(child: Text("Drawer"))),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          ListView.builder(
-              padding: const EdgeInsets.all(8),
-              itemCount: _listItems.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  height: 50,
-                  child: RaisedButton(
-                    child: Text(_listItems[index]["Text"]),
-                    onPressed: () {
-                      _deleteItem(index);
-                    },
-                  ),
-                );
-              }),
-          TextButton(
-              child: Text('Alert'),
-              onPressed: () async {
-                return showDialog<void>(
-                  context: context,
-                  barrierDismissible: true, // user must tap button!
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      backgroundColor: HexColor('f5f3ef'),
-                      title: const Text('Food List'),
-                      content: StatefulBuilder(builder:
-                          (BuildContext context, StateSetter setState) {
-                        return new Container(
-                          child: RaisedButton(
-                            child: Text(_textex),
-                            onPressed: () {
-                              _textex = "ほげほげ";
-                            },
+      // Center から変更 スクロールできるようにする
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            // 3行
+            for (int x = 0; x < 3; x++)
+              Row(
+                children: [
+                  // 横に4個並べる  4を変えれば変わる
+                  // i = 0 +(x * 4) ・・・2列目なら4から、3列目なら8から
+                  // i < 4 * (x + 1) ・・・2列目なら8まで、3列目なら12まで
+                  for (int i = 0 + (x * 4); i < 4 * (x + 1); i++)
+                    if (listItems.length > i)
+                      Card(
+                        //margin: const EdgeInsets.all(10.0),
+                        child: Container(
+                          width: 180.0,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              ButtonTheme(
+                                child: ButtonBar(
+                                  children: [
+                                    Tooltip(
+                                      message: 'クリップボードから削除します。',
+                                      child: IconButton(
+                                        icon: const Icon(Icons.close,
+                                            color: Colors.black),
+                                        onPressed: () {
+                                          _deleteItem(i);
+                                        },
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Image.network(
+                                listItems[i]['image'],
+                                height: 120.0,
+                                width: 120.0,
+                              ),
+                              Text(
+                                listItems[i]['Text'],
+                                style: TextStyle(fontSize: 16),
+                              ),
+                            ],
                           ),
-                        );
-                      }),
-                    );
-                  },
-                );
-              }),
-        ],
+                        ),
+                      ),
+                ],
+              ),
+          ],
+        ),
       ),
+      // 比較リスト
       floatingActionButton: FloatingActionButton(
         backgroundColor: HexColor('ec9463'),
         tooltip: 'Increment',
         child: Icon(Icons.assignment, color: Colors.white),
         onPressed: () async {
+          // MyHomePageと同期をとるだけのsetState
+          setState(() {});
           return showDialog<void>(
             context: context,
             barrierDismissible: true, // user must tap button!
             builder: (BuildContext context) {
-              return AlertDialog(
-                backgroundColor: HexColor('f5f3ef'),
-                title: const Text('Food List'),
-                content: StatefulBuilder(
-                    builder: (BuildContext context, StateSetter setState) {
-                  return new Container(
-                    width: MediaQuery.of(context).size.width * .8,
-                    child: new GridView.builder(
-                        itemCount: _listItems.length,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 4,
-                        ),
-                        itemBuilder: (BuildContext context, int index) {
-                          return Card(
-                            //margin: const EdgeInsets.all(10.0),
-                            child: Column(
-                              mainAxisSize: MainAxisSize.max,
-                              children: [
-                                ButtonTheme(
-                                  child: ButtonBar(
-                                    children: [
-                                      Tooltip(
-                                        message: 'クリップボードから削除します。',
-                                        child: IconButton(
-                                          icon: const Icon(Icons.close,
-                                              color: Colors.black),
-                                          onPressed: () {
-                                            _deleteItem(index);
-                                          },
-                                        ),
-                                      )
-                                    ],
-                                  ),
-                                ),
-                                Image.network(
-                                  _listItems[index]['image'],
-                                  height: 120.0,
-                                  width: 120.0,
-                                ),
-                                Text(
-                                  _listItems[index]['Text'],
-                                  style: TextStyle(fontSize: 20),
-                                ),
-                              ],
-                            ),
-                          );
-                        }),
-                  );
-                }),
-                actions: <Widget>[
-                  Center(
-                    child: Row(
-                      // 均等配置
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Opacity(
-                          opacity: 0.0,
-                        ),
-                        Opacity(
-                          opacity: 0.0,
-                        ),
-                        Opacity(
-                          opacity: 0.0,
-                        ),
-                        OutlinedButton(
-                          child: const Text('比較'),
-                          style: TextButton.styleFrom(primary: Colors.black),
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                        Opacity(
-                          opacity: 0.0,
-                        ),
-                        TextButton.icon(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          icon: Icon(
-                            Icons.delete_outlined,
-                          ),
-                          label: Text('全体削除'),
-                          style: TextButton.styleFrom(
-                            primary: HexColor('8c6e63'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              );
+              //Statefulなダイアログのクラスを作成したので呼び出し
+              return MyDialog();
+
+              // return AlertDialog(
+              //   backgroundColor: HexColor('f5f3ef'),
+              //   title: const Text('Food List'),
+              //   content: Container(
+              //     width: MediaQuery.of(context).size.width * .8,
+              //     child: ListItemWidget = showListItems(), // 比較リストを返す関数
+              //   ),
+              //   actions: <Widget>[
+              //     Center(
+              //       child: Row(
+              //         // 均等配置
+              //         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              //         children: [
+              //           Opacity(
+              //             opacity: 0.0,
+              //           ),
+              //           Opacity(
+              //             opacity: 0.0,
+              //           ),
+              //           Opacity(
+              //             opacity: 0.0,
+              //           ),
+              //           OutlinedButton(
+              //             child: const Text('比較'),
+              //             style: TextButton.styleFrom(primary: Colors.black),
+              //             onPressed: () {
+              //               Navigator.of(context).pop();
+              //             },
+              //           ),
+              //           Opacity(
+              //             opacity: 0.0,
+              //           ),
+              //           TextButton.icon(
+              //             onPressed: () {
+              //               Navigator.of(context).pop();
+              //             },
+              //             icon: Icon(
+              //               Icons.delete_outlined,
+              //             ),
+              //             label: Text('全体削除'),
+              //             style: TextButton.styleFrom(
+              //               primary: HexColor('8c6e63'),
+              //             ),
+              //           ),
+              //         ],
+              //       ),
+              //     ),
+              //   ],
+              // );
             },
           );
         },
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+//Statefulなダイアログクラス
+class MyDialog extends StatefulWidget {
+  @override
+  _MyDialogState createState() => _MyDialogState();
+}
+
+class _MyDialogState extends State<MyDialog> {
+  Widget ListItemWidget = Text("");
+
+  SingleChildScrollView showListItems() {
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          // 3行
+          for (int x = 0; x < 3; x++)
+            Row(
+              children: [
+                // 横に4個並べる  4を変えれば変わる
+                // i = 0 +(x * 4) ・・・2列目なら4から、3列目なら8から
+                // i < 4 * (x + 1) ・・・2列目なら8まで、3列目なら12まで
+                for (int i = 0 + (x * 4); i < 4 * (x + 1); i++)
+                  if (listItems.length > i)
+                    Card(
+                      //margin: const EdgeInsets.all(10.0),
+                      child: Container(
+                        width: 180.0,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            ButtonTheme(
+                              child: ButtonBar(
+                                children: [
+                                  Tooltip(
+                                    message: 'クリップボードから削除します。',
+                                    child: IconButton(
+                                      icon: const Icon(Icons.close,
+                                          color: Colors.black),
+                                      onPressed: () {
+                                        _deleteItem(i);
+                                      },
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Image.network(
+                              listItems[i]['image'],
+                              height: 120.0,
+                              width: 120.0,
+                            ),
+                            Text(
+                              listItems[i]['Text'],
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+
+  void _deleteItem(i) {
+    setState(() {
+      listItems.removeAt(i); // 削除
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: HexColor('f5f3ef'),
+      title: const Text('Food List'),
+      content: Container(
+        width: MediaQuery.of(context).size.width * .6,
+        child: ListItemWidget = showListItems(), // 比較リストを返す関数
+      ),
+      actions: <Widget>[
+        Center(
+          child: Row(
+            // 均等配置
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Opacity(
+                opacity: 0.0,
+              ),
+              Opacity(
+                opacity: 0.0,
+              ),
+              Opacity(
+                opacity: 0.0,
+              ),
+              OutlinedButton(
+                child: const Text('比較'),
+                style: TextButton.styleFrom(primary: Colors.black),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+              Opacity(
+                opacity: 0.0,
+              ),
+              TextButton.icon(
+                onPressed: () {
+                  setState(() {
+                    listItems.removeRange(0, listItems.length);
+                  });
+                },
+                icon: Icon(
+                  Icons.delete_outlined,
+                ),
+                label: Text('全体削除'),
+                style: TextButton.styleFrom(
+                  primary: HexColor('8c6e63'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
