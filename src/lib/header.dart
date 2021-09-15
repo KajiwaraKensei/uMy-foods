789fac05d797461d8f0ddf83aa8df8c4b32d101b
TarouTanakaYokohama <57135586+TarouTanakaYokohama@users.noreply.github.com>
2021-09-15 13:24:33 +0900

    結合前

--- src/lib/header.dart
+++ src/lib/header.dart
@@ -8,68 +8,48 @@ class Header extends StatelessWidget with PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
-
-      @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Screen title'),
-        leading: _buildProfileIconButton(),
-      ),
-      body:  /* Main contents */
-    )
-  }
-
-  Widget _buildProfileIconButton() {
-    const iconSize = 32.0;
-    return IconButton(
-      icon: photoUrl == null
-          ? Icon(
-              Icons.account_circle,
-              size: iconSize,
-            )
-          : CircleAvatar(
-              backgroundImage: NetworkImage(photoUrl),
-              backgroundColor: Colors.transparent,
-              radius: iconSize / 2,
-            ),
-      onPressed: /* Action */,
-    );
-  }
     return AppBar(
-
-
-      // leadingWidth: 80,
-      // // titleSpacing: 300,
-      // title: TextField(
-      //   decoration: InputDecoration(
-      //     // hintText: "クチコミ・商品・ユーザを検索",
-      //     prefixIcon: Icon(Icons.search),
-      //     fillColor: Colors.white,
-      //     filled: true,
-      //     enabledBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(2.0),
-      //       borderSide: BorderSide(
-      //         color: Colors.black,
-      //       ),
-      //     ),
-      //     focusedBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(2.0),
-      //       borderSide: BorderSide(
-      //         color: Colors.black,
-      //       ),
-      //     ),
-      //     border: InputBorder.none,
-      //   ),
-      //   style: TextStyle(color: Colors.black),
-      // ),
-
-
-
-      // centerTitle: true,
-
+      leadingWidth: 80,
+      // titleSpacing: 300,
+      title: TextField(
+        decoration: InputDecoration(
+          // hintText: "クチコミ・商品・ユーザを検索",
+          prefixIcon: Icon(Icons.search),
+          fillColor: Colors.white,
+          filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(2.0),
+            borderSide: BorderSide(
+              color: Colors.black,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(2.0),
+            borderSide: BorderSide(
+              color: Colors.black,
+            ),
+          ),
+          border: InputBorder.none,
+        ),
+        style: TextStyle(color: Colors.black),
+      ),
+      centerTitle: true,
+      toolbarHeight: 150,
+      leading: IconButton(
+        icon: CircleAvatar(
+          backgroundImage: AssetImage('images/uMyFoods_icon.png'),
+          // backgroundColor: Colors.transparent, // 背景色
+          // radius: 90, // 表示したいサイズの半径を指定
+        ),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MyApp(),
+              ));
+        },
+      ),
+      
       // actions: [
       //   IconButton(
       //     icon: CircleAvatar(
